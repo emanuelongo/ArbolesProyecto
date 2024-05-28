3e0f919d6f0971b3b27fe2b35227cb790b64d228
emanuelongo <emanuel1001cano@gmail.com>
2024-05-28 09:13:00 -0500

    Changes

---
+++ Main.py/Persona.py
@@ -0,0 +1,7 @@
+class Persona:
+    def __init__(self, numeroPaciente: str, genero: str, nombre: str, edad: int, triaje: int):
+        self.numeroPaciente = numeroPaciente
+        self.genero = genero
+        self.nombre = nombre
+        self.edad = edad
+        self.triaje = triaje
